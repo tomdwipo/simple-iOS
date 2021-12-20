53571704ab352ba3f4234a0271d7c17f5cf222ef
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-12-20 12:18:48 +0700

    first init

---
+++ simple-iOS/simple-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-iOS
+//
+//  Created by Tommy on 20/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
